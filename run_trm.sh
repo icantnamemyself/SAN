15ca9286ca7cebec7a30ff1d9039ac297b4a63d2
zhiding <865755707@qq.com>
2023-10-30 10:59:00 +0800

    update training scripts

--- run_trm.sh
+++ run_trm.sh
@@ -11,220 +11,218 @@ station_type=adaptive
 features=M
 gpu=0
 
-#for model_name in Autoformer FEDformer; do # Informer Transformer FEDformer
-#  for pred_len in 96 192 336 720; do
-#    python -u run_longExp.py \
-#      --is_training 1 \
-#      --root_path ./datasets/exchange_rate \
-#      --data_path exchange_rate.csv \
-#      --model_id exchange_96_$pred_len \
-#      --model $model_name \
-#      --data custom \
-#      --features $features \
-#      --seq_len $seq_len \
-#      --label_len $label_len \
-#      --pred_len $pred_len \
-#      --e_layers 2 \
-#      --d_layers 1 \
-#      --factor 3 \
-#      --enc_in 8 \
-#      --dec_in 8 \
-#      --c_out 8 \
-#      --des 'Exp' \
-#      --gpu $gpu \
-#      --station_type $station_type \
-#      --period_len 6 \
-#      --station_lr 0.001 \
-#      --itr 3 >logs/LongForecasting/$model_name'_exchange_rate_'$pred_len.log
-#
-#    python -u run_longExp.py \
-#      --is_training 1 \
-#      --root_path ./datasets/electricity \
-#      --data_path electricity.csv \
-#      --model_id electricity_96_$pred_len \
-#      --model $model_name \
-#      --data custom \
-#      --features $features \
-#      --seq_len $seq_len \
-#      --label_len $label_len \
-#      --pred_len $pred_len \
-#      --e_layers 2 \
-#      --d_layers 1 \
-#      --factor 3 \
-#      --enc_in 321 \
-#      --dec_in 321 \
-#      --c_out 321 \
-#      --des 'Exp' \
-#      --gpu $gpu \
-#      --station_type $station_type \
-#      --period_len 24 \
-#      --itr 3 >logs/LongForecasting/$model_name'_electricity_'$pred_len.log
-#
-#      python -u run_longExp.py \
-#        --is_training 1 \
-#        --root_path ./datasets/traffic \
-#        --data_path traffic.csv \
-#        --model_id traffic_96_$pred_len \
-#        --model $model_name \
-#        --data custom \
-#        --features $features \
-#        --seq_len $seq_len \
-#        --label_len $label_len \
-#        --pred_len $pred_len \
-#        --e_layers 2 \
-#        --d_layers 1 \
-#        --factor 3 \
-#        --enc_in 862 \
-#        --dec_in 862 \
-#        --c_out 862 \
-#        --des 'Exp' \
-#        --gpu $gpu \
-#        --station_type $station_type \
-#        --period_len 24 \
-#        --itr 3 >logs/LongForecasting/$model_name'_traffic_'$pred_len.log
-#
-#      python -u run_longExp.py \
-#        --is_training 1 \
-#        --root_path ./datasets/weather \
-#        --data_path weather.csv \
-#        --model_id weather_96_$pred_len \
-#        --model $model_name \
-#        --data custom \
-#        --features $features \
-#        --seq_len $seq_len \
-#        --label_len $label_len \
-#        --pred_len $pred_len \
-#        --e_layers 2 \
-#        --d_layers 1 \
-#        --factor 3 \
-#        --enc_in 21 \
-#        --dec_in 21 \
-#        --c_out 21 \
-#        --des 'Exp' \
-#        --gpu $gpu \
-#        --station_type $station_type \
-#        --period_len 12 \
-#        --itr 3  >logs/LongForecasting/$model_name'_weather_'$pred_len.log
-#
-#      python -u run_longExp.py \
-#          --is_training 1 \
-#          --root_path ./datasets/ETT-small \
-#          --data_path ETTh1.csv \
-#          --model_id ETTh1_96_$pred_len \
-#          --model $model_name \
-#          --data ETTh1 \
-#          --features $features \
-#          --seq_len $seq_len \
-#          --label_len $label_len \
-#          --pred_len $pred_len \
-#          --e_layers 2 \
-#          --d_layers 1 \
-#          --factor 3 \
-#          --enc_in 7 \
-#          --dec_in 7 \
-#          --c_out 7 \
-#          --des 'Exp' \
-#          --gpu $gpu \
-#          --station_type $station_type \
-#          --period_len 24 \
-#          --itr 3  >logs/LongForecasting/$model_name'_Etth1_'$pred_len.log
-#
-#      python -u run_longExp.py \
-#          --is_training 1 \
-#          --root_path ./datasets/ETT-small \
-#          --data_path ETTh2.csv \
-#          --model_id ETTh2_96_$pred_len \
-#          --model $model_name \
-#          --data ETTh2 \
-#          --features $features \
-#          --seq_len $seq_len \
-#          --label_len $label_len \
-#          --pred_len $pred_len \
-#          --e_layers 2 \
-#          --d_layers 1 \
-#          --factor 3 \
-#          --enc_in 7 \
-#          --dec_in 7 \
-#          --c_out 7 \
-#          --des 'Exp' \
-#          --gpu $gpu \
-#          --station_type $station_type \
-#          --period_len 24 \
-#          --itr 3  >logs/LongForecasting/$model_name'_Etth2_'$pred_len.log
-#
-#      python -u run_longExp.py \
-#          --is_training 1 \
-#          --root_path ./datasets/ETT-small \
-#          --data_path ETTm1.csv \
-#          --model_id ETTm1_96_$pred_len \
-#          --model $model_name \
-#          --data ETTm1 \
-#          --features $features \
-#          --seq_len $seq_len \
-#          --label_len $label_len \
-#          --pred_len $pred_len \
-#          --e_layers 2 \
-#          --d_layers 1 \
-#          --factor 3 \
-#          --enc_in 7 \
-#          --dec_in 7 \
-#          --c_out 7 \
-#          --des 'Exp' \
-#          --gpu $gpu \
-#          --station_type $station_type \
-#          --period_len 12 \
-#          --itr 3  >logs/LongForecasting/$model_name'_Ettm1_'$pred_len.log
-#
-#      python -u run_longExp.py \
-#          --is_training 1 \
-#          --root_path ./datasets/ETT-small \
-#          --data_path ETTm2.csv \
-#          --model_id ETTm2_96_$pred_len \
-#          --model $model_name \
-#          --data ETTm2 \
-#          --features $features \
-#          --seq_len $seq_len \
-#          --label_len $label_len \
-#          --pred_len $pred_len \
-#          --e_layers 2 \
-#          --d_layers 1 \
-#          --factor 3 \
-#          --enc_in 7 \
-#          --dec_in 7 \
-#          --c_out 7 \
-#          --des 'Exp' \
-#          --gpu $gpu \
-#          --station_type $station_type \
-#          --period_len 12 \
-#          --itr 3  >logs/LongForecasting/$model_name'_Ettm2_'$pred_len.log
-#  done
-#done
+for model_name in Autoformer FEDformer Informer Transformer; do
+  for pred_len in 96 192 336 720; do
+    python -u run_longExp.py \
+      --is_training 1 \
+      --root_path ./datasets/exchange_rate \
+      --data_path exchange_rate.csv \
+      --model_id exchange_96_$pred_len \
+      --model $model_name \
+      --data custom \
+      --features $features \
+      --seq_len $seq_len \
+      --label_len $label_len \
+      --pred_len $pred_len \
+      --e_layers 2 \
+      --d_layers 1 \
+      --factor 3 \
+      --enc_in 8 \
+      --dec_in 8 \
+      --c_out 8 \
+      --des 'Exp' \
+      --gpu $gpu \
+      --station_type $station_type \
+      --period_len 6 \
+      --station_lr 0.001 \
+      --itr 3 >logs/LongForecasting/$model_name'_exchange_rate_'$pred_len.log
 
-for model_name in Autoformer FEDformer # Informer Transformer FEDformer
-do
-for pred_len in 24 36 48 60
-do
-  python -u run_longExp.py \
-    --is_training 1 \
-    --root_path ./datasets/illness \
-    --data_path national_illness.csv \
-    --model_id ili_36_$pred_len \
-    --model $model_name \
-    --data custom \
-    --features $features \
-    --seq_len 36 \
-    --label_len 18 \
-    --pred_len $pred_len \
-    --e_layers 2 \
-    --d_layers 1 \
-    --factor 3 \
-    --enc_in 7 \
-    --dec_in 7 \
-    --c_out 7 \
-    --des 'Exp' \
-    --period_len 6 \
-    --gpu $gpu \
-    --station_type None \
-    --itr 3 >logs/LongForecasting/$model_name'_ili_none_'$pred_len.log
+    python -u run_longExp.py \
+      --is_training 1 \
+      --root_path ./datasets/electricity \
+      --data_path electricity.csv \
+      --model_id electricity_96_$pred_len \
+      --model $model_name \
+      --data custom \
+      --features $features \
+      --seq_len $seq_len \
+      --label_len $label_len \
+      --pred_len $pred_len \
+      --e_layers 2 \
+      --d_layers 1 \
+      --factor 3 \
+      --enc_in 321 \
+      --dec_in 321 \
+      --c_out 321 \
+      --des 'Exp' \
+      --gpu $gpu \
+      --station_type $station_type \
+      --period_len 24 \
+      --itr 3 >logs/LongForecasting/$model_name'_electricity_'$pred_len.log
+
+    python -u run_longExp.py \
+      --is_training 1 \
+      --root_path ./datasets/traffic \
+      --data_path traffic.csv \
+      --model_id traffic_96_$pred_len \
+      --model $model_name \
+      --data custom \
+      --features $features \
+      --seq_len $seq_len \
+      --label_len $label_len \
+      --pred_len $pred_len \
+      --e_layers 2 \
+      --d_layers 1 \
+      --factor 3 \
+      --enc_in 862 \
+      --dec_in 862 \
+      --c_out 862 \
+      --des 'Exp' \
+      --gpu $gpu \
+      --station_type $station_type \
+      --period_len 24 \
+      --itr 3 >logs/LongForecasting/$model_name'_traffic_'$pred_len.log
+
+    python -u run_longExp.py \
+      --is_training 1 \
+      --root_path ./datasets/weather \
+      --data_path weather.csv \
+      --model_id weather_96_$pred_len \
+      --model $model_name \
+      --data custom \
+      --features $features \
+      --seq_len $seq_len \
+      --label_len $label_len \
+      --pred_len $pred_len \
+      --e_layers 2 \
+      --d_layers 1 \
+      --factor 3 \
+      --enc_in 21 \
+      --dec_in 21 \
+      --c_out 21 \
+      --des 'Exp' \
+      --gpu $gpu \
+      --station_type $station_type \
+      --period_len 12 \
+      --itr 3 >logs/LongForecasting/$model_name'_weather_'$pred_len.log
+
+    python -u run_longExp.py \
+      --is_training 1 \
+      --root_path ./datasets/ETT-small \
+      --data_path ETTh1.csv \
+      --model_id ETTh1_96_$pred_len \
+      --model $model_name \
+      --data ETTh1 \
+      --features $features \
+      --seq_len $seq_len \
+      --label_len $label_len \
+      --pred_len $pred_len \
+      --e_layers 2 \
+      --d_layers 1 \
+      --factor 3 \
+      --enc_in 7 \
+      --dec_in 7 \
+      --c_out 7 \
+      --des 'Exp' \
+      --gpu $gpu \
+      --station_type $station_type \
+      --period_len 24 \
+      --itr 3 >logs/LongForecasting/$model_name'_Etth1_'$pred_len.log
+
+    python -u run_longExp.py \
+      --is_training 1 \
+      --root_path ./datasets/ETT-small \
+      --data_path ETTh2.csv \
+      --model_id ETTh2_96_$pred_len \
+      --model $model_name \
+      --data ETTh2 \
+      --features $features \
+      --seq_len $seq_len \
+      --label_len $label_len \
+      --pred_len $pred_len \
+      --e_layers 2 \
+      --d_layers 1 \
+      --factor 3 \
+      --enc_in 7 \
+      --dec_in 7 \
+      --c_out 7 \
+      --des 'Exp' \
+      --gpu $gpu \
+      --station_type $station_type \
+      --period_len 24 \
+      --itr 3 >logs/LongForecasting/$model_name'_Etth2_'$pred_len.log
+
+    python -u run_longExp.py \
+      --is_training 1 \
+      --root_path ./datasets/ETT-small \
+      --data_path ETTm1.csv \
+      --model_id ETTm1_96_$pred_len \
+      --model $model_name \
+      --data ETTm1 \
+      --features $features \
+      --seq_len $seq_len \
+      --label_len $label_len \
+      --pred_len $pred_len \
+      --e_layers 2 \
+      --d_layers 1 \
+      --factor 3 \
+      --enc_in 7 \
+      --dec_in 7 \
+      --c_out 7 \
+      --des 'Exp' \
+      --gpu $gpu \
+      --station_type $station_type \
+      --period_len 12 \
+      --itr 3 >logs/LongForecasting/$model_name'_Ettm1_'$pred_len.log
+
+    python -u run_longExp.py \
+      --is_training 1 \
+      --root_path ./datasets/ETT-small \
+      --data_path ETTm2.csv \
+      --model_id ETTm2_96_$pred_len \
+      --model $model_name \
+      --data ETTm2 \
+      --features $features \
+      --seq_len $seq_len \
+      --label_len $label_len \
+      --pred_len $pred_len \
+      --e_layers 2 \
+      --d_layers 1 \
+      --factor 3 \
+      --enc_in 7 \
+      --dec_in 7 \
+      --c_out 7 \
+      --des 'Exp' \
+      --gpu $gpu \
+      --station_type $station_type \
+      --period_len 12 \
+      --itr 3 >logs/LongForecasting/$model_name'_Ettm2_'$pred_len.log
+  done
 done
+
+for model_name in Autoformer FEDformer Informer Transformer; do
+  for pred_len in 24 36 48 60; do
+    python -u run_longExp.py \
+      --is_training 1 \
+      --root_path ./datasets/illness \
+      --data_path national_illness.csv \
+      --model_id ili_36_$pred_len \
+      --model $model_name \
+      --data custom \
+      --features $features \
+      --seq_len 36 \
+      --label_len 18 \
+      --pred_len $pred_len \
+      --e_layers 2 \
+      --d_layers 1 \
+      --factor 3 \
+      --enc_in 7 \
+      --dec_in 7 \
+      --c_out 7 \
+      --des 'Exp' \
+      --period_len 6 \
+      --gpu $gpu \
+      --station_type $station_type \
+      --itr 3 >logs/LongForecasting/$model_name'_ili_none_'$pred_len.log
+  done
 done
